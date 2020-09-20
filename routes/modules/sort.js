a9d49b9a3a6c7ed809aaaa111d57b1a24189f941
Darric <darric.lu@gmail.com>
2020-09-20 20:54:27 +0800

    Add sort function

--- routes/modules/sort.js
+++ routes/modules/sort.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const record = require('../../models/record')
+const categories = require('../../models/category')
 
 router.get('/:category', (req, res) => {
   const sort = req.params.category
-  console.log(sort)
+  record.find()
+    .lean()
+    .then(record => {
+      const filterRecord = record.filter(e => e.category === sort)
+      let total = 0
+      filterRecord.forEach(e => {
+        total += Number(e.amount)
+      })
+      categories.find()
+        .lean()
+        .sort({ _id: 'asc' })
+        .then(category => {
+          res.render('index', { total, category, record: filterRecord })
+        })
+    })
+    .catch(error => console.log(error))
 })
 
 module.exports = router
