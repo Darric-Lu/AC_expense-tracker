501743cbe7bf1d21f7b2a4ab3410d8f24ffe8b8e
Darric <darric.lu@gmail.com>
2020-09-20 17:45:29 +0800

    Add create page

--- routes/modules/record.js
+++ routes/modules/record.js
@@ -1,8 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const record = require('../../models/record')
+const categories = require('../../models/category')
 
 router.get('/create', (req, res) => {
-  res.render('new')
+  categories.find()
+    .lean()
+    .sort({ _id: 'asc' })
+    .then(category => {
+      res.render('new', { category })
+    })
 })
 
+router.post('/', (req, res) => {
+  const { name, date, amount, category, description } = req.body
+  let categoryArry = category.split(',')
+  record.create({
+    name,
+    date,
+    amount,
+    category: categoryArry[0],
+    icon: categoryArry[1],
+    description
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
 module.exports = router
